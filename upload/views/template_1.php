2ef02568c8bf1f2540d15476f87ebb11bd78e8ce
Valentin <valtervalentin1@hotmail.com>
2021-01-21 20:49:51 -0200

    #006e: Sistema Multi-linguagem (2/2) - Feito adaptação para Boostrap 4 a index.php

--- upload/views/template_1.php
+++ upload/views/template_1.php
@@ -1,57 +1,46 @@
-<!doctype html>
-<html lang="pt-br">
+<!DOCTYPE html>
+<html>
     <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />       
         <title>Loja 3.0</title>
         <link rel="shortcut icon" href="<?php echo BASE_URL; ?>assets/images/favicon.png" />
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css">        
-
-        <!-- CSS -->
+        <link href="//fonts.googleapis.com/css?family=Helvetica%20Neue:300,400,600,700,800" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css" type="text/css" />
-
     </head>
     <body>
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-                <ul class="navbar-nav mr-auto">   
-                    <li class="nav-item active">
-                        <a class="nav-link" href="<?php echo BASE_URL; ?>"><?php $this->lang->get('HOME'); ?><span class="sr-only">(página atual)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL; ?>contact"><?php $this->lang->get('CONTACT'); ?></a>
-                    </li>
+        <nav class="navbar topnav">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="<?php echo BASE_URL; ?>"><?php $this->lang->get('HOME'); ?></a></li>
+                    <li><a href="<?php echo BASE_URL; ?>contact"><?php $this->lang->get('CONTACT'); ?></a></li>
                 </ul>
-                <ul class="navbar-nav navbar-right">   
-                    <li class="nav-item dropdown">                       
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <?php $this->lang->get('LANGUAGE'); ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php $this->lang->get('LANGUAGE'); ?>
+                            <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <p class="topnav-text">
+                                    <img src="<?php echo BASE_URL; ?>assets/images/en-gb.png" /><a href="<?php echo BASE_URL; ?>lang/set/en"> English</a>
+                                </p>
+                            </li>
+                            <li>
+                                <p class="topnav-text">
+                                    <img src="<?php echo BASE_URL; ?>assets/images/pt-br.png" /><a href="<?php echo BASE_URL; ?>lang/set/pt-br"> Português</a>
+                                </p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASE_URL; ?>login">
+                            <?php $this->lang->get('LOGIN'); ?>
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <p class="topnav-text">
-                                <img src="<?php echo BASE_URL; ?>assets/images/en-gb.png" /><a href="<?php echo BASE_URL; ?>lang/set/en"> English</a>
-                            <p>
-                            <p class="topnav-text">
-                                <img src="<?php echo BASE_URL; ?>assets/images/pt-br.png" /><a href="<?php echo BASE_URL; ?>lang/set/pt-br"> Português</a>
-                            <p>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL; ?>login"><?php $this->lang->get('LOGIN'); ?></a>
                     </li>
                 </ul>
-            </div>            
+            </div>
         </nav>
-
         <header>
             <div class="container">
                 <div class="row">
@@ -70,7 +59,7 @@
                                 </select>
                                 <input type="submit" value="" />
                             </form>
-                        </div> 
+                        </div>
                     </div>
                     <div class="col-sm-3">
                         <a href="<?php echo BASE_URL; ?>cart">
@@ -84,44 +73,30 @@
                                 </div>
                             </div>
                         </a>
-                    </div>                  
-                </div>                
+                    </div>
+                </div>
             </div>
         </header>
-
-        <!--nav class="navbar navbar-expand-md bg-primary navbar-dark "-->
-        <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #00A7EC;">    
-
-            <!-- Toggler/collapsibe Button -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <!-- Navbar links -->
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#"><?php $this->lang->get('SELECTCATEGORY'); ?></a>
-                    </li>
-
-                    <!-- Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                            CATEGORIA 1
-                        </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Subcategoria</a>
-                            <a class="dropdown-item" href="#">Subcategoria</a>
-                            <a class="dropdown-item" href="#">Subcategoria</a>
-                        </div>
-                    </li>
-
-
-                </ul>
-            </div>
-        </nav>
-
+        <div class="categoryarea">
+            <nav class="navbar">
+                <div class="container">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <?php $this->lang->get('SELECTCATEGORY'); ?>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Page 1-1</a></li>
+                                <li><a href="#">Page 1-2</a></li>
+                                <li><a href="#">Page 1-3</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Categoria X</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
         <section>
             <div class="container">
                 <div class="row">
@@ -133,7 +108,7 @@
                             </div>
 
                             <div class="widget">
-                                <h1>Produtos em Destaque</h1>
+                                <h1><?php $this->lang->get('FEATUREDPRODUCTS'); ?></h1>
                                 <div class="widget_body">
                                     ...
                                 </div>
@@ -144,7 +119,6 @@
                 </div>
             </div>
         </section>
-
         <footer>
             <div class="container">
                 <div class="row">
@@ -177,14 +151,12 @@
             <div class="subarea">
                 <div class="container">
                     <div class="row">
-                        <div class="mx-auto" style="width: 600px;">
-                            <div class="col-xs-12 col-sm-8 col-sm-offset-2 no-padding">
-                                <form method="POST">
-                                    <input class="subemail" name="email" placeholder="<?php $this->lang->get('SUBSCRIBETEXT'); ?>">
-                                    <input type="submit" value="<?php $this->lang->get('SUBSCRIBEBUTTON'); ?>" />
-                                </form>
-                            </div>
-                        </div>    
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 no-padding">
+                            <form method="POST">
+                                <input class="subemail" name="email" placeholder="<?php $this->lang->get('SUBSCRIBETEXT'); ?>">
+                                <input type="submit" value="<?php $this->lang->get('SUBSCRIBEBUTTON'); ?>" />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -192,7 +164,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-4">
-                            <a href="<?php echo BASE_URL; ?>" /><img width="150" src="<?php echo BASE_URL; ?>assets/images/logo.svg" /></a><br/><br/>
+                            <a href="<?php echo BASE_URL; ?>"><img width="150" src="<?php echo BASE_URL; ?>assets/images/logo.svg" /></a><br/><br/>
                             <strong>Slogan da Loja Virtual</strong><br/><br/>
                             Endereço da Loja Virtual
                         </div>
@@ -239,7 +211,7 @@
             <div class="copyright">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-6">© <span>Loja 3.0</span> - <?php $this->lang->get('ALLRIGHTRESERVED'); ?></div>
+                        <div class="col-sm-6">© <span>Loja 3.0</span> - <?php $this->lang->get('ALLRIGHTRESERVED'); ?>.</div>
                         <div class="col-sm-6">
                             <div class="payments">
                                 <img src="<?php echo BASE_URL; ?>assets/images/visa.png" />
@@ -253,10 +225,8 @@
             </div>
         </footer>
         <script type="text/javascript">var BASE_URL = '<?php echo BASE_URL; ?>';</script>
-        
-        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-        <script src="<?php echo BASE_URL; ?>assets/js/jquery-3.5.1.slim.min.js"></script>
-        <script src="<?php echo BASE_URL; ?>assets/js/bootstrap.bundle.min.js"></script>
-
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/script.js"></script>
     </body>
 </html>
